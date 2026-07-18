cd16249ae9a84810f0e386c67192ccc86d441258
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-18 16:05:12 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,25 +1,3 @@
-import sqlite3
-from config import DATABASE_PATH
+from flask_sqlalchemy import SQLAlchemy
 
-
-def get_connection():
-    conn = sqlite3.connect(DATABASE_PATH)
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
-def create_tables():
-    conn = get_connection()
-
-    conn.execute("""
-        CREATE TABLE IF NOT EXISTS urls (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            original_url TEXT NOT NULL,
-            short_code TEXT UNIQUE NOT NULL,
-            clicks INTEGER DEFAULT 0,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    """)
-
-    conn.commit()
-    conn.close()
+db = SQLAlchemy()
