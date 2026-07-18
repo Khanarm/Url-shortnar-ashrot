9df48dd057f9db91cd20bb75939fa99c74dfb539
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-18 21:30:19 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,7 @@
-
+function copyLink(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        alert("✅ Link copied successfully!");
+    }).catch(() => {
+        alert("❌ Failed to copy link.");
+    });
+}
