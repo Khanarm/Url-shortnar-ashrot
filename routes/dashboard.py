9ce3c77926091b6b46b3a5bd5154921afb956038
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-18 21:17:13 +0530

    Create dashboard.py

---
+++ routes/dashboard.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template
+from models import URL
+
+dashboard_bp = Blueprint("dashboard", __name__)
+
+
+@dashboard_bp.route("/dashboard")
+def dashboard():
+
+    urls = URL.query.all()
+
+    total_links = len(urls)
+
+    total_clicks = sum(url.clicks for url in urls)
+
+    return render_template(
+        "dashboard.html",
+        urls=urls,
+        total_links=total_links,
+        total_clicks=total_clicks
+    )
