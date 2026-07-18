51e3d138626cfbf2607e4ea4bc65f6b7b2711e6f
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-18 16:07:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from config import Config
+from database import db
+from models import URL
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
