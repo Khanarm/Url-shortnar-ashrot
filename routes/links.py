cad37460b363421910b8622dc03824facf0d302d
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-19 12:09:19 +0530

    Update links.py

--- routes/links.py
+++ routes/links.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect
+from flask import Blueprint, redirect, render_template
 from database import db
 from models import URL
 
@@ -7,6 +7,31 @@ links_bp = Blueprint("links", __name__)
 
 @links_bp.route("/<short_code>")
 def redirect_url(short_code):
+
+    url = URL.query.filter_by(short_code=short_code).first()
+
+    if not url:
+        return "Link not found", 404
+
+    return redirect(f"/wait/{short_code}")
+
+
+@links_bp.route("/wait/<short_code>")
+def wait_page(short_code):
+
+    url = URL.query.filter_by(short_code=short_code).first()
+
+    if not url:
+        return "Link not found", 404
+
+    return render_template(
+        "wait.html",
+        short_code=short_code
+    )
+
+
+@links_bp.route("/go/<short_code>")
+def go(short_code):
 
     url = URL.query.filter_by(short_code=short_code).first()
 
