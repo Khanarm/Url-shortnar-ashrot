016acfae3c79f1fb723105f3c0235f8c15948df9
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-19 10:22:56 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -14,3 +14,19 @@ function copyLink(button){
     },2000);
 
 }
+
+function generateQR(){
+
+    let link = document.getElementById("shortLink").value;
+
+    let qrUrl = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" 
+                + encodeURIComponent(link);
+
+
+    let qrBox = document.getElementById("qrCode");
+
+    qrBox.innerHTML = `
+        <img src="${qrUrl}" alt="QR Code">
+    `;
+
+}
