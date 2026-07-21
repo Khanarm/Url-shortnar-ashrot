616b7b477099f80d679a48cccd82b67b8295eb87
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-21 13:12:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SECRET_KEY = "change-this-secret-key"
+    SECRET_KEY = os.getenv("SECRET_KEY", "change-this-secret-key")
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
+    MONGO_URI = os.getenv(
+        "MONGO_URI",
+        "mongodb+srv://USERNAME:PASSWORD@cluster0.xxxxx.mongodb.net/url_shortener?retryWrites=true&w=majority"
+    )
 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:5000")
