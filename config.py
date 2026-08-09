b2652ee454189115e73b22c0a5c9c3c39043129a
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-08-09 19:30:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
 
     MONGO_URI = os.getenv(
         "MONGO_URI",
-        "mongodb+srv://ahadansaridmk_db_user:575751an@cluster0.its13au.mongodb.net/?appName=Cluster0"
+        ""
     )
 
     BASE_URL = os.getenv("BASE_URL", "database_name")
