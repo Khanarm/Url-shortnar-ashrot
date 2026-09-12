b31568ec962edf5d430c91fb6b6dd2198538407f
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-09-12 21:38:40 +0530

    Add users and withdrawals collections to mongo.py

--- mongo.py
+++ mongo.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
 from config import Config
 
+
 client = MongoClient(Config.MONGO_URI)
 
 db = client["url_shortener"]
 
 urls = db["urls"]
+users = db["users"]
+withdrawals = db["withdrawals"]
